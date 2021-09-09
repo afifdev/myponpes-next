ddcf446e3e1999d03f52dd6bc3b78d4b7c98a760
afifdev <afifudin.fdn@gmail.com>
2021-09-09 14:56:21 +0700

    adding variable

---
+++ next.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  env: {
+    MONGO_URI:
+      "mongodb://afifudin:hIeC2nLWT1VurroJ@cluster0-shard-00-00.jy7lh.mongodb.net:27017,cluster0-shard-00-01.jy7lh.mongodb.net:27017,cluster0-shard-00-02.jy7lh.mongodb.net:27017/myponpes?ssl=true&replicaSet=atlas-tmut6w-shard-0&authSource=admin&retryWrites=true&w=majority",
+    // MONGO_URI:
+    //   "mongodb://afifudin:hIeC2nLWT1VurroJ@cluster0-shard-00-00.jy7lh.mongodb.net:27017,cluster0-shard-00-01.jy7lh.mongodb.net:27017,cluster0-shard-00-02.jy7lh.mongodb.net:27017/myponpes?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true",
+    JWT_SECRET: "hIeC2nLWT1VurroJ",
+  },
+};
