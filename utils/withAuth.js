e2f996578697ed5dcf04c960967fe025c6c35ca4
afifdev <afifudin.fdn@gmail.com>
2021-09-10 19:49:22 +0700

    Adding admin, teacher, santri in user

--- utils/withAuth.js
+++ utils/withAuth.js
@@ -1,18 +1,25 @@
 import { useContext } from "react";
 import { UserContext } from "./useUser";
 import Home from "@pages/index";
+import Layout from "@components/Layout";
 
 const withAuth = (Component, role) => {
   const Auth = (props) => {
     const user = useContext(UserContext);
-    if (!user.role && user.isDone) {
-      return <Home />;
-    } else if (user.role === 2 && role === "admin") {
+    if (!user.isDone) {
+      return (
+        <Layout>
+          <div>Loading...from withAuth</div>
+        </Layout>
+      );
+    } else if (user.role === 3 && role === "admin") {
       return <Component {...props} />;
-    } else if (user.role === 1 && role === "user") {
+    } else if (user.role === 2 && role === "teacher") {
+      return <Component {...props} />;
+    } else if (user.role === 1 && role === "santri") {
       return <Component {...props} />;
     } else {
-      return <div>Loading...from withAuth</div>;
+      return <Home />;
     }
   };
 
